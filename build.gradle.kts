3a5a4370539ad53284002226b849d54220b75be7
ulgenzade <ulgenzade@users.noreply.github.com>
2026-09-16 20:34:14 +0300

    fix: add -Xskip-metadata-version-check to bypass Kotlin 2.4 metadata incompatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,8 @@ subprojects {
                 freeCompilerArgs.addAll(
                     "-Xno-call-assertions",
                     "-Xno-param-assertions",
-                    "-Xno-receiver-assertions"
+                    "-Xno-receiver-assertions",
+                    "-Xskip-metadata-version-check"
                 )
             }
         }
